24ea5757ef40fda3a745ea3df821292cc5fa1f1e
Matt Buchs <110405782+MattBuchs@users.noreply.github.com>
2024-05-01 19:01:34 +0200

    phone navbar updated

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 
 export default function Nav({ display, setDisplay }) {
+    const [size, setSize] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+    });
+
+    useEffect(() => {
+        function handleResize() {
+            setSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+
+        window.addEventListener("resize", handleResize);
+
+        // Nettoyage de l'écouteur d'événement lors du démontage du composant
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
     return (
         <header className="bg-gray-200 select-none">
             <div
                 className={`flex justify-between items-center text-white px-6 h-14 ${
-                    display ? "w-full" : "w-48"
-                } border-b shadow bg-indigo-900 after:absolute after:w-0 after:h-0 after:left-48 after:border-t-[27px] after:border-b-[28px] after:border-l-[40px] after:border-l-indigo-900 after:border-t-transparent after:border-b-transparent cursor-pointer transition`}
-                onClick={() => setDisplay(!display)}
+                    display && size.width > 640
+                        ? "sm:w-full cursor-pointer"
+                        : "sm:w-48"
+                } w-full border-b shadow bg-indigo-900 sm:after:absolute sm:after:w-0 sm:after:h-0 sm:after:left-48 sm:after:border-t-[27px] sm:after:border-b-[28px] sm:after:border-l-[40px] sm:after:border-l-indigo-900 sm:after:border-t-transparent sm:after:border-b-transparent transition`}
+                onClick={() => size.width > 640 && setDisplay(!display)}
             >
                 <h1
                     className="text-xl mb-1"
@@ -16,36 +38,37 @@ export default function Nav({ display, setDisplay }) {
                     <Link to={"/"}>PlaySkroma</Link>
                 </h1>
 
-                {display && (
-                    <nav onClick={(e) => e.stopPropagation()}>
-                        <ul className="flex text-lg">
-                            <li>
-                                <NavLink
-                                    to={"/"}
-                                    className={({ isActive }) =>
-                                        isActive
-                                            ? "underline underline-offset-4"
-                                            : ""
-                                    }
-                                >
-                                    Home
-                                </NavLink>
-                            </li>
-                            <li className="ml-4">
-                                <NavLink
-                                    to={"/checkers"}
-                                    className={({ isActive }) =>
-                                        isActive
-                                            ? "underline underline-offset-4"
-                                            : ""
-                                    }
-                                >
-                                    Checkers
-                                </NavLink>
-                            </li>
-                        </ul>
-                    </nav>
-                )}
+                {display ||
+                    (size.width <= 640 && (
+                        <nav onClick={(e) => e.stopPropagation()}>
+                            <ul className="flex text-lg">
+                                <li>
+                                    <NavLink
+                                        to={"/"}
+                                        className={({ isActive }) =>
+                                            isActive
+                                                ? "underline underline-offset-4"
+                                                : ""
+                                        }
+                                    >
+                                        Home
+                                    </NavLink>
+                                </li>
+                                <li className="ml-4">
+                                    <NavLink
+                                        to={"/checkers"}
+                                        className={({ isActive }) =>
+                                            isActive
+                                                ? "underline underline-offset-4"
+                                                : ""
+                                        }
+                                    >
+                                        Checkers
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        </nav>
+                    ))}
             </div>
         </header>
     );

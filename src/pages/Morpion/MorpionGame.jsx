1fd3e862a87254b8f4b5fda27310e123c8f7259f
Matt Buchs <110405782+MattBuchs@users.noreply.github.com>
2024-08-30 13:44:24 +0200

    adding a score and a replay button to the tic-tac-toe

--- src/pages/Morpion/MorpionGame.jsx
+++ src/pages/Morpion/MorpionGame.jsx
@@ -5,16 +5,34 @@ import AddNicknames from "../../components/Mopion/Modal/AddNicknames";
 export default function MorpionGame() {
     const tableRef = useRef();
     const [player, setPlayer] = useState(1);
+    const [startingPlayer, setStartingPlayer] = useState(1);
     const [playersName, setPlayersName] = useState({
         player1: "X",
         player2: "O",
     });
+    const [score, setScore] = useState({
+        player1: 0,
+        player2: 0,
+    });
     const [winner, setWinner] = useState("");
     const [modal, setModal] = useState(false);
+    const [showReplayButton, setShowReplayButton] = useState(false);
     const [table, setTable] = useState([
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0],
+        [
+            { value: 0, className: "" },
+            { value: 0, className: "" },
+            { value: 0, className: "" },
+        ],
+        [
+            { value: 0, className: "" },
+            { value: 0, className: "" },
+            { value: 0, className: "" },
+        ],
+        [
+            { value: 0, className: "" },
+            { value: 0, className: "" },
+            { value: 0, className: "" },
+        ],
     ]);
 
     const displayContent = (value) => {
@@ -23,31 +41,63 @@ export default function MorpionGame() {
         if (value === -1) return "O";
     };
 
-    const handleGame = (e, indexRow, indexCol) => {
+    const handleGame = (indexRow, indexCol) => {
         if (winner === "") {
-            const value = e.target.textContent;
+            const cell = table[indexRow][indexCol];
 
-            if (!value) {
-                const newTable = [...table];
-                const newRow = [...newTable[indexRow]];
-
-                newRow[indexCol] = player === 1 ? 1 : -1;
-                newTable[indexRow] = newRow;
+            if (cell.value === 0) {
+                const newTable = table.map((row, rIdx) =>
+                    row.map((cell, cIdx) => {
+                        if (rIdx === indexRow && cIdx === indexCol) {
+                            return {
+                                value: player === 1 ? 1 : -1,
+                                className:
+                                    player === 1
+                                        ? "text-red-700"
+                                        : "text-blue-700",
+                            };
+                        }
+                        return cell;
+                    })
+                );
 
                 setTable(newTable);
-
                 setPlayer(player === 1 ? 2 : 1);
             }
         }
     };
 
-    const resetGame = () => {
-        setPlayer(1);
+    const resetGame = (isReplay) => {
+        if (isReplay) {
+            setStartingPlayer(startingPlayer === 1 ? 2 : 1);
+            setPlayer(startingPlayer === 1 ? 2 : 1);
+        } else {
+            setPlayer(1);
+            setScore({
+                player1: 0,
+                player2: 0,
+            });
+        }
+
+        tableRef.current.classList.remove("blur-sm");
+        setShowReplayButton(false);
         setWinner("");
         setTable([
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0],
+            [
+                { value: 0, className: "" },
+                { value: 0, className: "" },
+                { value: 0, className: "" },
+            ],
+            [
+                { value: 0, className: "" },
+                { value: 0, className: "" },
+                { value: 0, className: "" },
+            ],
+            [
+                { value: 0, className: "" },
+                { value: 0, className: "" },
+                { value: 0, className: "" },
+            ],
         ]);
     };
 
@@ -56,12 +106,20 @@ export default function MorpionGame() {
         if (sum === -3) return setWinner("circle");
     };
 
+    const checkEquality = () => {
+        return table.every((row) => row.every((cell) => cell.value !== 0));
+    };
+
     useEffect(() => {
+        const isEquality = checkEquality();
+
+        if (isEquality) setWinner("equality");
+
         for (let i = 0; i < table.length; i++) {
             let sum = 0;
 
             for (let j = 0; j < table.length; j++) {
-                sum += table[i][j];
+                sum += table[i][j].value;
                 checkSum(sum);
             }
         }
@@ -70,65 +128,105 @@ export default function MorpionGame() {
             let sum = 0;
 
             for (let j = 0; j < table.length; j++) {
-                sum += table[j][i];
+                sum += table[j][i].value;
                 checkSum(sum);
             }
         }
 
-        const rightDiad = table[0][0] + table[1][1] + table[2][2];
+        const rightDiad =
+            table[0][0].value + table[1][1].value + table[2][2].value;
         checkSum(rightDiad);
 
-        const leftDiad = table[0][2] + table[1][1] + table[2][0];
+        const leftDiad =
+            table[0][2].value + table[1][1].value + table[2][0].value;
         checkSum(leftDiad);
     }, [table]);
 
+    useEffect(() => {
+        if (winner) {
+            console.log(winner);
+
+            if (winner === "cross")
+                setScore({ ...score, player1: (score.player1 += 1) });
+            if (winner === "circle")
+                setScore({ ...score, player2: (score.player2 += 1) });
+
+            setShowReplayButton(true);
+            tableRef.current.classList.add("blur-sm");
+        }
+    }, [winner]);
+
     return (
         <div className="h-full">
+            {console.log(score.player1)}
             <h2 className="text-center text-4xl font-bold mt-6">Morpion</h2>
+            <p className="text-center text-3xl mt-4">
+                <span className="text-red-700 font-bold">{score.player1}</span>{" "}
+                /{" "}
+                <span className="text-blue-700 font-bold">{score.player2}</span>
+            </p>
 
-            <div className="flex flex-col justify-center items-center mt-36">
+            <div className="flex flex-col justify-center items-center mt-28">
                 {winner === "" && (
                     <p className="mb-2 text-xl">
                         Player{" "}
-                        {player === 1
-                            ? playersName.player1
-                            : playersName.player2}{" "}
+                        <span
+                            className={`font-bold ${
+                                player === 1 ? "text-red-700" : "text-blue-700"
+                            }`}
+                        >
+                            {player === 1
+                                ? playersName.player1
+                                : playersName.player2}
+                        </span>{" "}
                         turn
                     </p>
                 )}
                 {winner && winner === "equality" && (
-                    <p className="mb-2 text-xl">Equality</p>
+                    <p className="mb-2 text-xl">Equality !</p>
                 )}
                 {winner && winner !== "equality" && (
                     <p className="mb-2 text-xl">
                         Player{" "}
-                        {winner === "cross"
-                            ? playersName.player1
-                            : playersName.player2}{" "}
+                        <span
+                            className={`font-bold ${
+                                winner === "cross"
+                                    ? "text-red-700"
+                                    : "text-blue-700"
+                            }`}
+                        >
+                            {winner === "cross"
+                                ? playersName.player1
+                                : playersName.player2}
+                        </span>{" "}
                         wins !
                     </p>
                 )}
+                {showReplayButton && (
+                    <button
+                        onClick={() => resetGame(true)}
+                        className="bg-blue-700 px-6 py-1.5 rounded text-white absolute z-10 text-2xl border border-gray-600 shadow-lg  hover:bg-blue-800"
+                    >
+                        Replay
+                    </button>
+                )}
                 <table
                     ref={tableRef}
-                    className="border-2 border-gray-600 w-[300px] h-[300px] mb-4 select-none"
+                    className="morpion border-2 border-gray-600 w-[300px] h-[300px] mb-4 select-none"
                 >
                     <tbody>
                         {table &&
                             table.map((row, indexRow) => (
                                 <tr key={indexRow}>
-                                    {row.map((box, indexCol) => (
+                                    {row.map((cell, indexCol) => (
                                         <td
                                             key={`${indexRow}-${indexCol}`}
-                                            onClick={(e) =>
-                                                handleGame(
-                                                    e,
-                                                    indexRow,
-                                                    indexCol
-                                                )
+                                            onClick={() =>
+                                                handleGame(indexRow, indexCol)
                                             }
-                                            className="w-1/3 h-1/3 border border-gray-800 text-center text-6xl cursor-pointer"
+                                            className={`w-1/3 h-1/3 border border-gray-800 text-center text-6xl cursor-pointer ${cell.className}`}
                                         >
-                                            {displayContent(box)}
+                                            {displayContent(cell.value)}
                                         </td>
                                     ))}
                                 </tr>
@@ -137,7 +235,7 @@ export default function MorpionGame() {
                 </table>
                 <div>
                     <button
-                        onClick={resetGame}
+                        onClick={() => resetGame(false)}
                         className="bg-blue-600 text-white px-4 py-1 rounded mr-2"
                     >
                         Reset
